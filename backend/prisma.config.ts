fcbead9a6f2d67cb51c5d27f375de754119419da
Joel Hounmenou <jesugojoelhounmenou@gmail.com>
2026-05-14 17:19:44 +0100

    Suppression prisma.config.ts

--- backend/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-// This file ensures Prisma knows exactly where to look for your schema and DB URL
-import 'dotenv/config';
-
-export default {
-  schema: './prisma/schema.prisma',
-  migrationsPath: './prisma/migrations'
-};
